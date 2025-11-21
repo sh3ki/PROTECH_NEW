b715cdcec312a3a6b242e5f9d4a576e56df9440f
sh3ki <shekaigarcia@gmail.com>
2025-11-21 12:44:02 +0800

    Update settings.py

--- PROTECH/settings.py
+++ PROTECH/settings.py
@@ -142,6 +142,16 @@ AUTH_USER_MODEL = 'PROTECHAPP.CustomUser'
 # Face recognition tuning
 FACE_MATCH_THRESHOLD = 0.65
 
+# Email Configuration
+EMAIL_BACKEND = config('EMAIL_BACKEND', default='django.core.mail.backends.smtp.EmailBackend')
+EMAIL_HOST = config('EMAIL_HOST', default='smtp.gmail.com')
+EMAIL_PORT = config('EMAIL_PORT', default=587, cast=int)
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=True, cast=bool)
+EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
+DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL', default='PROTECH Attendance System <noreply@protech.com>')
+
+
 # Configure logging
 LOGGING = {
     'version': 1,
