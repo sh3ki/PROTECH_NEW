fbbda847fba3ce12b22aaa652374f36a310919f9
sh3ki <shekaigarcia@gmail.com>
2026-01-13 03:25:46 +0800

    Updated admin.py

--- PROTECHAPP/admin.py
+++ PROTECHAPP/admin.py
@@ -23,6 +23,36 @@ class BackupLogAdmin(admin.ModelAdmin):
     readonly_fields = ['filename', 'filepath', 'file_size_bytes', 'file_size_mb', 'backup_type', 'status', 'error_message', 'initiated_by', 'created_at']
     ordering = ['-created_at']
 
+
+@admin.register(SystemSettings)
+class SystemSettingsAdmin(admin.ModelAdmin):
+    list_display = ['id', 'attendance_mode', 'recognition_display_mode', 'spoof_proof_enabled', 'email_notifications_enabled', 'sms_notifications_enabled']
+    list_editable = ['spoof_proof_enabled']
+    fieldsets = (
+        ('Face Recognition', {
+            'fields': (
+                'face_confidence_min', 'face_confidence_max', 'recognition_display_mode', 'spoof_proof_enabled'
+            )
+        }),
+        ('Attendance Windows', {
+            'fields': (
+                'attendance_time_in', 'attendance_time_out', 'attendance_mode',
+                'first_class_start_time', 'second_class_start_time', 'grace_period_minutes'
+            )
+        }),
+        ('Notifications', {
+            'fields': (
+                'email_notifications_enabled', 'sms_notifications_enabled'
+            )
+        }),
+        ('SMS Gateway', {
+            'fields': ('sms_gateway_url', 'sms_api_key', 'sms_sender_id')
+        }),
+        ('Email Gateway', {
+            'fields': ('email_host', 'email_port', 'email_username', 'email_password')
+        }),
+    )
+
 admin.site.register(CustomUser, CustomUserAdmin)
 admin.site.register(Grade)
 admin.site.register(Section)
@@ -39,5 +69,4 @@ admin.site.register(Chat)
 admin.site.register(Message)
 admin.site.register(ChatParticipant)
 admin.site.register(MessageNotification)
-admin.site.register(SystemSettings)
 admin.site.register(BackupLog, BackupLogAdmin)
